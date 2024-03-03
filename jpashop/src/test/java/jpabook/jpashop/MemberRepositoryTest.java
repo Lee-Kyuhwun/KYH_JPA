7336325e65b879216d7ccd605f7530e0b9141d5c
이규훈 <dlrbgns0904@sju.ac.kr>
2024-03-04 08:40:28 +0900

    feat: 엔티티 추가

--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop;
 
 import jakarta.transaction.Transactional;
+import jpabook.jpashop.domain.Member;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;

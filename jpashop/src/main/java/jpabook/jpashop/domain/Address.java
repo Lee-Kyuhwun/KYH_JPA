50b24927e203d0b584c5e6bbcbe43bfb857753cc
이규훈 <wxyz0904@naver.com>
2024-03-15 22:17:16 +0900

    feat: JPA 활용 1 엔티티 클래스 개발2까지

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -12,4 +12,12 @@ public class Address {
     private String street;
     private String zipcode;
 
+    protected Address() { // JPA 스펙상 엔티티나 임베디드 타입은 기본 생성자를 public 또는 protected로 설정해야 한다.
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }

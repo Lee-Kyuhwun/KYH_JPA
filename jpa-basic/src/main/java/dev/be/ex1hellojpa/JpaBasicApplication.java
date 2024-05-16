0ee5960eb346fcb1b4e27a265358cbd0c5924dc2
이규훈 <dlrbgns0904@sju.ac.kr>
2024-05-16 22:48:18 +0900

    feat: jpa 기본 프로젝트 생성

--- jpa-basic/src/main/java/dev/be/ex1hellojpa/JpaBasicApplication.java
+++ jpa-basic/src/main/java/dev/be/ex1hellojpa/JpaBasicApplication.java
@@ -1,13 +1,43 @@
 package dev.be.ex1hellojpa;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
 
-@SpringBootApplication
 public class JpaBasicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JpaBasicApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");// hello라는 이름의 persistence unit을 찾아서 EntityManagerFactory를 만들어줌
+        EntityManager entityManager = emf.createEntityManager();
+		EntityTransaction tx = entityManager.getTransaction();
+		tx.begin();
+        try{
+            Member findMember = entityManager.find(Member.class, 1L);
+
+            entityManager.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList()
+                    .forEach(System.out::println);
+            findMember.setName("HelloJPA");
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            entityManager.close();
+        }
+
+		Member member = new Member();
+		member.setId(1L);
+		member.setName("HelloA");
+		entityManager.persist(member);
+		tx.commit();
+
+        entityManager.close();
+        emf.close();
+
+    }
 
 }

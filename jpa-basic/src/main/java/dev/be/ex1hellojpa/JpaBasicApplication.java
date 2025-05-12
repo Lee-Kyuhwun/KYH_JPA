7ccbae3c634e0dfd00e9701438ba04c519848fa6
LeeKyuHwun <wxyz0904@naver.com>
2025-05-12 23:20:54 +0900

    feat: 김영한 jpa 기본 단방향 매핑

--- jpa-basic/src/main/java/dev/be/ex1hellojpa/JpaBasicApplication.java
+++ jpa-basic/src/main/java/dev/be/ex1hellojpa/JpaBasicApplication.java
@@ -10,39 +10,32 @@ public class JpaBasicApplication {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");// hello라는 이름의 persistence unit을 찾아서 EntityManagerFactory를 만들어줌
-        EntityManager entityManager = emf.createEntityManager();
-		EntityTransaction tx = entityManager.getTransaction();
+        EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
 		tx.begin();
         try{
 
+            Team team = new Team();
+            team.setName("Team 1");
+            em.persist(team);
 
             Member member = new Member();
-            member.setRoleType(RoleType.USER);
+            member.setName("Member 1");
+            member.setTeam(team);
+            em.persist(member);
 
-            Member findMember = entityManager.find(Member.class, 1L);
-            Class<Member> memberClass = Member.class;
-            entityManager.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList()
-                    .forEach(System.out::println);
-            tx.commit();
+            Member findMember = em.find(Member.class, member.getId());
+
+            Team findTeam = findMember.getTeam();
+
+
+
+
+
         }catch (Exception e){
+            e.printStackTrace();
             tx.rollback();
-        }finally {
-            entityManager.close();
         }
-        // JPQL 엔티티 객체를 대상으로 검색한다.
-        // SQL은 데이터 베이스 테이블을 대상으로 쿼리
-        // JPQL은 엔티티 객체를 대상으로 쿼리
-        // JPQL은 SQL을 추상화한 객체 지향 쿼리 언어
-		Member member = new Member();
-
-		entityManager.persist(member);
-		tx.commit();
-
-        entityManager.close();
-        emf.close();
 
     }
 
--- jpa-basic/src/main/java/dev/be/ex1hellojpa/Team.java
+++ jpa-basic/src/main/java/dev/be/ex1hellojpa/Team.java
@@ -3,14 +3,18 @@ package dev.be.ex1hellojpa;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Team {
 
     @Id
     private Long teamId;
 
-
+    private String name;
 
 
 }
